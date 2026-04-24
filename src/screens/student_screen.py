7a917c4797afb4156abad1691b02b4dab66658fb
varun saxena <saxenavarun119@gmail.com>
2026-04-24 19:08:40 +0530

    minor fixes

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -47,10 +47,10 @@ def student_dashboard():
     for log in logs:
         sid=log['subject_id']
         if sid not in stats_map:
-            stats_map['sid']={'total':0,'attended':0}
+            stats_map[sid]={'total':0,'attended':0}
         stats_map[sid]['total']+=1
 
-        if logs.get('is_present'):
+        if log.get('is_present'):
             stats_map[sid]['attended']+=1
     
     cols=st.columns(2)
